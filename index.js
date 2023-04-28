807b040b03da26effe3160d36f689afff55359d3
Shubham Singh <subhamsinghpg@gmail.com>
2023-04-28 12:13:54 +0530

    added route and a controller

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+app.use('/', require('./routes/index'));
+
 app.listen(port, function(err){
      if(err){
         console.log(`error occurred: ${err}`);
